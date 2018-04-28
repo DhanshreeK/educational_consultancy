9dc6cb44fd7e970be880df240b79ecaa3dcfa8f5
Dhanshree <dhanshree.j.kamble@gmail.com>
2018-04-28 14:35:00 +0530

    Revert "Question answer"

--- app/models/comment.rb
+++
@@ -1,3 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :student_detail
-end
--- app/models/student_answer.rb
+++
@@ -1,5 +0,0 @@
-class StudentAnswer < ApplicationRecord
-  belongs_to :student_detail , dependent: :destroy
-  belongs_to :question , dependent: :destroy
-  belongs_to :answer, dependent: :destroy
-end
